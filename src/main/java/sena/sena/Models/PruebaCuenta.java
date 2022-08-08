27ae626d7f68b06a13e97782b1f525079bc5f04c
stive <stiven23ospina@gmail.com>
2022-08-08 14:51:59 -0500

    Relational Cuenta and Cliente

--- src/main/java/sena/sena/Models/PruebaCuenta.java
+++ src/main/java/sena/sena/Models/PruebaCuenta.java
@@ -22,10 +22,42 @@ public class PruebaCuenta {
     private Integer id;
     
     @ManyToOne
+    @JoinColumn(name="idCuentafk")
     private Cuenta cuentas;
 
     @ManyToOne
+    @JoinColumn(name="idClientefk")
     private Cliente clientes;
+    public PruebaCuenta(){}
+    public PruebaCuenta(Integer id, Cuenta cuentas, Cliente clientes) {
+        this.id = id;
+        this.cuentas = cuentas;
+        this.clientes = clientes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cuenta getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(Cuenta cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public Cliente getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Cliente clientes) {
+        this.clientes = clientes;
+    }
 
     
 
